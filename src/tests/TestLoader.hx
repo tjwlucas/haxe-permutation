4de580a3eb4c27e82eba91e6b4933061a48919cf
Thomas Lucas <tom@thomaslucas.co.uk>
2022-04-14 15:00:53 +0000

    Add testing pipeline

--- src/tests/TestLoader.hx
+++ src/tests/TestLoader.hx
@@ -11,8 +11,10 @@ class TestLoader {
 	}
 
 	static function __init__() {
+		#if php
 		// PHP 8.1 compatibility workaround https://github.com/HaxeFoundation/haxe/issues/10502
 		untyped if (version_compare(PHP_VERSION, "8.1.0", ">=")) error_reporting(error_reporting() & ~E_DEPRECATED);
+		#end
 	}
 }
 
@@ -20,10 +22,19 @@ class NoExitReport extends utest.ui.text.PrintReport {
 	override function complete(result:utest.ui.common.PackageResult) {
 		this.result = result;
 
+		#if js
+		js.html.Console.log(this.getResults());
+		#elseif sys
 		Sys.println(this.getResults());
+		#end
+
 		instrument.coverage.Coverage.endCoverage();
-		if (!result.stats.isOk) {
+		if (!result.stats.isOk) {			
+			#if sys
 			Sys.exit(1);
+			#elseif js
+			js.Syntax.code('process.exit(1)');
+			#end
 		}
 	}
 }
